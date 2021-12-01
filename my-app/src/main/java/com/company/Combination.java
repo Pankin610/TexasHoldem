71d2b0720a72771cfec6e6469cabb91fa2e4ecbe
Pankin610 <pavel.sankin@student.uj.edu.pl>
2021-12-01 23:27:21 +0000

    combinations with bitsets

--- my-app/src/main/java/com/company/Combination.java
+++ my-app/src/main/java/com/company/Combination.java
@@ -6,30 +6,39 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Arrays;
 import java.util.List;
+import java.util.BitSet;
 
 public class Combination {
-    List<Card> cards;
+    BitSet cards_mask = new BitSet(52);
     int value;
 
     private Combination(Collection<Card> cards, int value) {
-        this.cards = new ArrayList<>(cards);
+        for (Card card : cards) {
+            cards_mask.set(card.index());
+        }
         this.value = value;
     }
 
-    public boolean isInHand(Collection<Card> hand) {
-        for (Card card : cards) {
-            if (!hand.contains(card)) {
-                return false;
-            }
-        }
-        return true;
+    public boolean isSubSet(BitSet st) {
+        BitSet nd = (BitSet)st.clone();
+        nd.and(cards_mask);
+        return nd.equals(cards_mask);
     }
 
+    static int max_val = 1;
     static public ArrayList<Combination> allCombinations = new ArrayList<>();
+    static void addCombination(Collection<Card> cards) {
+        allCombinations.add(new Combination(cards, max_val));
+        max_val += 1;
+    }
 
     static {
         for (Card card : Card.allCards) {
-            allCombinations.add(new Combination(Arrays.asList(card, card), 1));
+            addCombination(Arrays.asList(card));
         }
+        for (Card card : Card.allCards) {
+            addCombination(Arrays.asList(card, card));
+        }
+
     }
 }
